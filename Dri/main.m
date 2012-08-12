54fae59f1fbb3f34b6d383425c029f537c5eede2
okihara <okihara.masataka@dena.jp>
2012-08-12 23:26:24 +0900

    Initial Commit

---
+++ Dri/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Dri
+//
+//  Created by  on 12/08/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
