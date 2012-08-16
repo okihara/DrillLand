eb67e7890b82315a070c14827e57865cacfaa2ec
okihara <okihara.masataka@dena.jp>
2012-08-16 14:39:08 +0900

    add block class
    Signed-off-by: okihara <okihara.masataka@dena.jp>

---
+++ Dri/BlockBase.h
@@ -0,0 +1,13 @@
+//
+//  BlockBase.h
+//  Dri
+//
+//  Created by  on 12/08/16.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockBase : NSObject
+
+@end
---
+++ Dri/BlockBase.m
@@ -0,0 +1,13 @@
+//
+//  BlockBase.m
+//  Dri
+//
+//  Created by  on 12/08/16.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BlockBase.h"
+
+@implementation BlockBase
+
+@end
