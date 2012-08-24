2cf89dcd877b7c1854cd4874b4e40bcc2e426cd9
okihara <okihara.masataka@dena.jp>
2012-08-25 07:39:39 +0900

    animation works
    Signed-off-by: okihara <okihara.masataka@dena.jp>

--- Dri/src/DungeonPreloadScene.m
+++ Dri/src/DungeonPreloadScene.m
@@ -19,25 +19,85 @@
 	return scene;
 }
 
+-(CCSpriteFrame*)load_frame:(NSDictionary*)frame
+{
+    //int duration = [frame objectForKey:@"duration"];
+    NSArray* layer_list = [frame objectForKey:@"layer"];
+    NSDictionary* l1 = [layer_list objectAtIndex:0];
+    CCSpriteFrameCache* frame_cache = [CCSpriteFrameCache sharedSpriteFrameCache];
+    NSString* name = [l1 objectForKey:@"uv"];
+    return [frame_cache spriteFrameByName:name];
+}
+
+-(CCAnimation*)load_action:(NSDictionary*)jsonItem
+{
+    NSArray* frame_list = [jsonItem objectForKey:@"frame"];
+    NSUInteger count = [frame_list count];
+    NSMutableArray* sprite_frame_list = [NSMutableArray arrayWithCapacity:count];
+    for (NSDictionary* frame in frame_list) {
+        CCSpriteFrame* sprite_frame = [self load_frame:frame];
+        [sprite_frame_list addObject:sprite_frame];
+    }
+    return [CCAnimation animationWithSpriteFrames:sprite_frame_list delay:0.08f];
+}
+
+-(void)load_animation:(NSString*)filename
+{
+    NSString *path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:filename];
+    NSString *jsonData = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    id jsonItem = [jsonData JSONValue];
+    NSDictionary *action = [(NSDictionary*)jsonItem objectForKey:@"action"];
+    
+    CCAnimationCache* anim_cache = [CCAnimationCache sharedAnimationCache];
+    
+    for (NSString *key in [action keyEnumerator]) {
+        CCAnimation* anim = [self load_action:[action objectForKey:key]];
+        [anim_cache addAnimation:anim name:key];
+    }
+}
+
+//----------------------------------------------------------------------------------------
+
+-(void)load_sprite:(NSString*)filename
+{
+    CCSpriteFrameCache* frame_cache = [CCSpriteFrameCache sharedSpriteFrameCache];
+
+    NSString *path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:filename];
+    NSString *jsonData = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    id jsonItem = [jsonData JSONValue];
+    NSDictionary *frames = [(NSDictionary*)jsonItem objectForKey:@"frames"];
+
+    for (NSString* key in [frames keyEnumerator]) {
+        NSDictionary* fr = [[frames objectForKey:key] objectForKey:@"frame"];
+        float x = [(NSNumber*)[fr objectForKey:@"x"] floatValue];
+        float y = [(NSNumber*)[fr objectForKey:@"y"] floatValue];
+        float w = [(NSNumber*)[fr objectForKey:@"w"] floatValue];
+        float h = [(NSNumber*)[fr objectForKey:@"h"] floatValue];
+        CGRect rect = CGRectMake(x, y, w, h);
+        
+        [frame_cache addSpriteFrame:[CCSpriteFrame frameWithTextureFilename:@"link_f.png" rect:rect] name:key];
+    }
+}
+
 // on "init" you need to initialize your instance
 -(id) init
 {
 	if( (self=[super init]) ) {
+        [self load_sprite:@"link_f.json"];
+        [self load_animation:@"link.json"];
+        
+        CCAnimation *anim = [[CCAnimationCache sharedAnimationCache] animationByName:@"walk"];
+        
+        CCSprite *sp = [CCSprite spriteWithSpriteFrameName:@"link_f8.png"];
+        sp.position = ccp(160, 240);
+        sp.scale = 2.0;
+        [self addChild:sp];
+        
+        CCAction* act = [CCRepeatForever actionWithAction:[CCAnimate actionWithAnimation:anim]];
+        //CCAction *act = [CCMoveTo actionWithDuration:10 position:ccp(0, 0)];
+        [sp runAction:act];
 	}
 	return self;
-}
-
--(void)onEnter
-{
-    NSLog(@"preload on enter");
-    NSString *path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"anim01.json"];
-    NSString *jsonData = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-                          
-    id jsonItem = [jsonData JSONValue];  
-
-    //-------------------------------------------------
-    CCDirector* director = [CCDirector sharedDirector];
-    [director replaceScene:[DungeonScene scene]];
 }
 
 @end
