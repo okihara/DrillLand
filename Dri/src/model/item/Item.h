5f852fcc866e1d0d1c67e4c1378de1e85253124c
okihara <okihara.masataka@dena.jp>
2012-09-24 12:33:40 +0900

    アイテムモデル追加
    Signed-off-by: okihara <okihara.masataka@dena.jp>

---
+++ Dri/src/model/item/Item.h
@@ -0,0 +1,18 @@
+//
+//  Item.h
+//  Dri
+//
+//  Created by  on 12/09/24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Item : NSObject
+{
+    int atk;
+    int def;
+    BOOL is_equipment;
+}
+
+@end
---
+++ Dri/src/model/item/Item.m
@@ -0,0 +1,13 @@
+//
+//  Item.m
+//  Dri
+//
+//  Created by  on 12/09/24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+@end
