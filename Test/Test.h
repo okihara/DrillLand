38d28948513d614c69f68592d689f7a17cb0f0b7
okihara <okihara.masataka@dena.jp>
2012-12-24 11:52:36 +0900

    テスト追加
    Signed-off-by: okihara <okihara.masataka@dena.jp>

---
+++ Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by  on 12/12/24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Test : SenTestCase
+
+@end
---
+++ Test/Test.m
@@ -0,0 +1,32 @@
+//
+//  Test.m
+//  Test
+//
+//  Created by  on 12/12/24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Test");
+}
+
+@end
