e431eaf33517cf865eaa06278fabf9694769a1ef
Masataka Okihara <okihara.masataka@O-03347-MAC.local>
2013-09-14 15:13:00 +0900

    added xdmap test

---
+++ Test/TestObjectXDMap.h
@@ -0,0 +1,13 @@
+//
+//  TestObjectXDMap.h
+//  Dri
+//
+//  Created by Masataka Okihara on 2013/04/26.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestObjectXDMap : SenTestCase
+
+@end
---
+++ Test/TestObjectXDMap.m
@@ -0,0 +1,13 @@
+//
+//  TestObjectXDMap.m
+//  Dri
+//
+//  Created by Masataka Okihara on 2013/04/26.
+//
+//
+
+#import "TestObjectXDMap.h"
+
+@implementation TestObjectXDMap
+
+@end
